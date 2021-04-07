fd8298bf38e464eb1de523924583d7385d95d533
sulta <agdarbekovsultan@gmail.com>
2021-04-07 23:50:47 +0600

    Fix method create , search , update

--- src/main/java/kz/iitu/dao/StudentsDao.java
+++ src/main/java/kz/iitu/dao/StudentsDao.java
@@ -29,54 +29,61 @@ public class StudentsDao {
         }
 
     }
+    public static int id = 0;
+    private List<Student> students;
 
-    public List<Student> list() throws SQLException {
-        PreparedStatement insert_query = connection.prepareStatement("SELECT  * FROM student");
-        ResultSet resultSet = insert_query.executeQuery();
-        List<Student> students = new ArrayList<>();
-        while(resultSet.next()){
-            int a =  resultSet.getInt("id");
-            String s = resultSet.getString("name");
-            System.out.println(a + s);
-            students.add(new Student(a, s));
-        }
-       return students;
+    public List<Student> list() {
+     students = new ArrayList<>();
+     try{
+         PreparedStatement statement = connection.prepareStatement("SELECT * FROM student");
+       ResultSet  resultSet = statement.executeQuery();
+       while(resultSet.next()){
+           int i = resultSet.getInt("id");
+           String s = resultSet.getString("name");
+           System.out.println(i + s);
+           students.add(new Student(i ,s));
+       }
+     } catch (SQLException throwables) {
+         throwables.printStackTrace();
+     }
+     return students;
     }
 
-    public Student search(int id) throws SQLException {
-        List<Student> student = new ArrayList<>();
-        PreparedStatement insert_query = connection.prepareStatement("SELECT * FROM student where id = ?");
-         insert_query.setString(1,"name");
-       // insert_query.executeQuery();
-        ResultSet resultSet = insert_query.executeQuery();
-        while(resultSet.next()){
-            int a =  resultSet.getInt(id);
-            String s = resultSet.getString("name");
-            System.out.println(a + s);
-            student.add(new Student(a, s));
+    public Student search(int id)  {
+        Student student = new Student();
+        try{
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM student where id = ?");
+            statement.setInt(1,id);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                student.setId(resultSet.getInt(1));
+                student.setName(resultSet.getString(2));
+            }
+        } catch (SQLException e ){
+            e.printStackTrace();
         }
-        return (Student) student;
+        return student;
     }
 
     public void create(Student newStudent) throws SQLException {
-        PreparedStatement insert_query = connection.prepareStatement("INSERT Into student(id , name) values (? , ?)");
-        insert_query.setInt(1,newStudent.getId());
-        insert_query.setString(2,"name");
+        PreparedStatement insert_query = connection.prepareStatement("INSERT INTO student (id , name) VALUES (? , ?)");
+        insert_query.setInt(1,id++);
+        insert_query.setString(2, newStudent.getName());
         insert_query.executeUpdate();
 
     }
 
     public void update(int id, Student updated_student) throws SQLException {
         PreparedStatement insert_query = connection.prepareStatement("UPDATE student SET name = ? where id = ?");
-        insert_query.setInt(1,id);
-        insert_query.setString(2,updated_student.getName());
+        insert_query.setString(1, updated_student.getName());
+        insert_query.setInt(2,id);
         insert_query.executeUpdate();
     }
 
     public void delete(int id) throws SQLException {
-        PreparedStatement insert_query = connection.prepareStatement("DELETE from student where id = ?");
-        insert_query.setInt(1,id);
-        insert_query.executeUpdate();
-
+     PreparedStatement statement = connection.prepareStatement("DELETE FROM student where id = ?");
+     statement.setInt(1,id);
+     statement.executeUpdate();
     }
+
 }
